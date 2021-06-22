c648beae2818357b8e03f8e78cb1d0652942b581
Viviane Miranda <viviane.miranda@nubank.com.br>
2021-06-22 10:53:58 -0300

    adding Lista de transferencias button and path

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -1,6 +1,7 @@
 import 'package:bytebank/screens/dashboard/saldo-card.dart';
 import 'package:bytebank/screens/deposito/formulario.dart';
 import 'package:bytebank/screens/transferencia/formulario.dart';
+import 'package:bytebank/screens/transferencia/lista.dart';
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
@@ -46,6 +47,18 @@ class Dashboard extends StatelessWidget {
               ),
             ],
           ),
+          ElevatedButton(
+              child: Text('Transferências'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return ListaTransferencias();
+                    },
+                  ),
+                );
+              }),
         ],
       ),
     );
